3465e719805e03a20881621eb9e31fad59b026f3
yocumnatwit <yocumn@wit.edu>
2025-11-25 15:43:24 -0500

    Setup timeline.tsx

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -4,6 +4,7 @@ import {
     VerticalTimelineElement
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
+import {NavLink} from "react-router";
 
 export function meta({}: Route.MetaArgs) {
     return [
@@ -34,7 +35,7 @@ export default function Home() {
 
                     <div className="mt-6 flex flex-wrap gap-3">
                         <a
-                            className="inline-block rounded-full bg-blue-600 text-white px-4 py-2 text-sm hover:opacity-90"
+                            className="inline-block rounded-full border border-gray-200 text-white px-4 py-2 text-sm hover:opacity-90"
                             href="https://www.english-heritage.org.uk/"
                             target="_blank"
                             rel="noreferrer"
@@ -42,7 +43,7 @@ export default function Home() {
                             English Heritage
                         </a>
                         <a
-                            className="inline-block rounded-full border border-gray-200 px-4 py-2 text-sm hover:bg-gray-100"
+                            className="inline-block rounded-full border border-gray-200 text-white px-4 py-2 text-sm hover:opacity-90"
                             href="https://www.britishmuseum.org/"
                             target="_blank"
                             rel="noreferrer"
@@ -50,12 +51,19 @@ export default function Home() {
                             British Museum
                         </a>
                         <a
-                            className="inline-block rounded-full border border-gray-200 px-4 py-2 text-sm hover:bg-gray-100"
+                            className="inline-block rounded-full border border-gray-200 text-white px-4 py-2 text-sm hover:opacity-90"
                             href="https://www.vindolanda.com/"
                             target="_blank"
                             rel="noreferrer"
                         >
                             Vindolanda Trust
+                        </a>
+                        <a
+                            className="inline-block rounded-full border border-gray-200 text-white px-4 py-2 text-sm hover:opacity-90"
+                        >
+                            <NavLink to="/timeline" end>
+                            The Timeline
+                            </NavLink>
                         </a>
                     </div>
                 </div>
@@ -205,172 +213,6 @@ export default function Home() {
                 </ul>
             </section>
 
-            <VerticalTimeline>
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--work"
-                    contentStyle={{
-                        background: "rgb(33, 150, 243)",
-                        color: "#fff"
-                    }}
-                    contentArrowStyle={{
-                        borderRight: "7px solid  rgb(33, 150, 243)"
-                    }}
-                    date="001"
-                    iconStyle={{
-                        background: "rgb(33, 150, 243)",
-                        color: "#fff"
-                    }}
-                    //icon={<WorkIcon />}
-                >
-                    <h3 className="vertical-timeline-element-title">
-                        Pre Roman Britain
-                    </h3>
-                    <h4 className="vertical-timeline-element-subtitle">
-                        Context (if needed)
-                    </h4>
-                    <p>
-                        Some information
-                    </p>
-                </VerticalTimelineElement>
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--work"
-                    date="000"
-                    iconStyle={{
-                        background: "rgb(33, 150, 243)",
-                        color: "#fff"
-                    }}
-                    contentStyle={{
-                        background: "rgb(33, 150, 243)",
-                        color: "#fff"
-                    }}
-                    //icon={<WorkIcon />}
-                >
-                    <h3 className="vertical-timeline-element-title">
-                        First Contact
-                    </h3>
-                    <h4 className="vertical-timeline-element-subtitle">
-                        Caesarian Incursions
-                    </h4>
-                    <p>
-                        summary / bullet list
-                    </p>
-                </VerticalTimelineElement>
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--work"
-                    date="100"
-                    iconStyle={{
-                        background: "rgb(33, 150, 243)",
-                        color: "#fff"
-                    }}
-                    contentStyle={{
-                        background: "rgb(33, 150, 243)",
-                        color: "#fff"
-                    }}
-                    //icon={<WorkIcon />}
-                >
-                    <h3 className="vertical-timeline-element-title">
-                        Claudius Invasion
-                    </h3>
-                    <h4 className="vertical-timeline-element-subtitle">
-                        2 battles
-                    </h4>
-                    <p>
-                        summary / bullet list
-                    </p>
-                </VerticalTimelineElement>
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--work"
-                    date="200"
-                    iconStyle={{
-                        background: "rgb(33, 150, 243)",
-                        color: "#fff"
-                    }}
-                    contentStyle={{
-                        background: "rgb(33, 150, 243)",
-                        color: "#fff"
-                    }}
-                    //icon={<WorkIcon />}
-                >
-                    <h3 className="vertical-timeline-element-title">
-                        Roman Rule Established
-                    </h3>
-                    <h4 className="vertical-timeline-element-subtitle">
-                        Londinium
-                    </h4>
-                    <p>
-                        summary / bullet list
-                    </p>
-                </VerticalTimelineElement>
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--education"
-                    date="300"
-                    iconStyle={{
-                        background: "rgb(33, 150, 243)",
-                        color: "#fff"
-                    }}
-                    contentStyle={{
-                        background: "rgb(33, 150, 243)",
-                        color: "#fff"
-                    }}
-                    //icon={<SchoolIcon />}
-                >
-                    <h3 className="vertical-timeline-element-title">
-                        Scotland Occupation Attempt
-                    </h3>
-                    <h4 className="vertical-timeline-element-subtitle">
-                        The scots
-                    </h4>
-                    <p>
-                        summary / bullet list
-                    </p>
-                </VerticalTimelineElement>
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--education"
-                    date="400"
-                    iconStyle={{
-                        background: "rgb(33, 150, 243)",
-                        color: "#fff"
-                    }}
-                    contentStyle={{
-                        background: "rgb(33, 150, 243)",
-                        color: "#fff"
-                    }}
-                    //icon={<SchoolIcon />}
-                >
-                    <h3 className="vertical-timeline-element-title">
-                        End of Roman Rule
-                    </h3>
-                    <h4 className="vertical-timeline-element-subtitle">
-                        no more Romans
-                    </h4>
-                    <p>
-                        summary / bullet list
-                    </p>
-                </VerticalTimelineElement>
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--education"
-                    date="500"
-                    iconStyle={{
-                        background: "rgb(33, 150, 243)",
-                        color: "#fff"
-                    }}
-                    contentStyle={{
-                        background: "rgb(33, 150, 243)",
-                        color: "#fff"
-                    }}
-                    //icon={<SchoolIcon />}
-                >
-                    <h3 className="vertical-timeline-element-title">
-                        Post Roman Period
-                    </h3>
-                    <h4 className="vertical-timeline-element-subtitle">
-                        All roads lead to London
-                    </h4>
-                    <p>
-                        summary / bullet list
-                    </p>
-                </VerticalTimelineElement>
-            </VerticalTimeline>
         </main>
     );
 }
