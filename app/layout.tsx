82a03438ad67039954f74a4e370aa3b5ad33b816
Lucas Bacchi <lucasbacchi22@gmail.com>
2025-12-07 20:33:50 -0500

    Enhance home page content with two more links

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,13 +24,14 @@ export function Layout({ children }: { children: React.ReactNode }) {
                     <Outlet />
                 </main>
                 <footer className="m-8 text-center text-sm text-gray-500">
-                    <p>&copy; 2025 Archaeology of Roman Britain</p>
+                    <p>&copy; 2025 Archaeology of Roman Britain Project Team</p>
                     <br />
-                    <p className="mb-4">
+                    <p>
                         Created by Lucas Bacchi, Noah Yocum, Matt Chiu, and
                         Keirston Artis for CSAS 2000 at Wentworth Institute of
                         Technology.
                     </p>
+                    <br />
                     <NavLink to={"works-cited"} className="p-4">
                         Works Cited Page
                     </NavLink>
